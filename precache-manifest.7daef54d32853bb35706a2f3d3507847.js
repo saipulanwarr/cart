c1daae1547cae2df202f2b1403532b97a924f82d
saipulanwarr <saipul0994@gmail.com>
2020-04-02 16:23:45 +0700

    Updates

---
+++ precache-manifest.7daef54d32853bb35706a2f3d3507847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67b66eda2e785af6f8d726793c49a7d",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "e76bbf8c81343a90a7dd",
+    "url": "/cart/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "143ccc58955e7086f2b6",
+    "url": "/cart/static/css/main.2daea198.chunk.css"
+  },
+  {
+    "revision": "e76bbf8c81343a90a7dd",
+    "url": "/cart/static/js/2.982a037d.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/cart/static/js/2.982a037d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143ccc58955e7086f2b6",
+    "url": "/cart/static/js/main.fd7bea2b.chunk.js"
+  },
+  {
+    "revision": "28c96e057adc07ea6b5d",
+    "url": "/cart/static/js/runtime-main.c4d7cfd0.js"
+  }
+]);
